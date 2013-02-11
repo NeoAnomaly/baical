22544561dfb8ed07cf832c96dc5257f0f2f4737d
Riant.Z <Riant.Z@gmail.com>
2013-02-11 20:11:03 +0000

    P7:  Added: example for telemetry

--- Tests/Example/Main.cpp
+++ Tests/Example/Main.cpp
@@ -22,17 +22,19 @@
 
 #include "P7_Client.h"
 #include "P7_Trace.h"
+#include "P7_Telemetry.h"
 
 
 int main(int i_iArgC, char* i_pArgV[])
 {
-    IP7_Client *l_pClient  = NULL;
-    IP7_Trace  *l_pTrace_1 = NULL;
-    IP7_Trace  *l_pTrace_2 = NULL;
-    tUINT32     l_dwIdx    = 0;
+    IP7_Client    *l_pClient    = NULL;
+    IP7_Trace     *l_pTrace_1   = NULL;
+    IP7_Trace     *l_pTrace_2   = NULL;
+    tUINT32        l_dwIdx      = 0;
+    IP7_Telemetry *l_pTelemetry = NULL;
+    tUINT8         l_pTID       = 0;
 
-    //printf("P7.Trace example application\n");
-
+    //create P7 client object
     l_pClient = P7_Create_Client(0);
 
     if (NULL == l_pClient)
@@ -40,18 +42,34 @@ int main(int i_iArgC, char* i_pArgV[])
         goto l_lblExit;
     }
 
-    l_pTrace_1 = P7_Create_Trace(l_pClient, TM("Test channel 1"));
+    //create P7 telemetry object
+    l_pTelemetry = P7_Create_Telemetry(l_pClient, TM("Telemetry channel 1"));
+    if (NULL == l_pTelemetry)
+    {
+        goto l_lblExit;
+    }
+
+    //register telemetry counter, it has values in range 0 ... 1023
+    if (FALSE == l_pTelemetry->Create(TM("Test counter"), 0, 1023, 1000, 1, &l_pTID))
+    {
+        goto l_lblExit;
+    }
+
+    //create P7 trace object 1
+    l_pTrace_1 = P7_Create_Trace(l_pClient, TM("Trace channel 1"));
     if (NULL == l_pTrace_1)
     {
         goto l_lblExit;
     }
 
-    l_pTrace_2 = P7_Create_Trace(l_pClient, TM("Test channel 2"));
+    //create P7 trace object 2
+    l_pTrace_2 = P7_Create_Trace(l_pClient, TM("Trace channel 2"));
     if (NULL == l_pTrace_2)
     {
         goto l_lblExit;
     }
 
+    //send few trace messages
     l_pTrace_1->P7_TRACE(0, TM("Test trace message #%d"), l_dwIdx ++);
     l_pTrace_1->P7_INFO(0, TM("Test info message #%d"), l_dwIdx ++);
     l_pTrace_1->P7_DEBUG(0, TM("Test debug message #%d"), l_dwIdx ++);
@@ -61,7 +79,18 @@ int main(int i_iArgC, char* i_pArgV[])
     
     l_pTrace_2->P7_QTRACE(1, 0, TM("Test quick trace on channel %d"), 2);
 
+    //send 1 million telemetry samples
+    for (tUINT64 l_qwI = 0ULL; l_qwI < 1000000ULL; l_qwI ++)
+    {
+        l_pTelemetry->Add(l_pTID, (l_qwI & 0x3FF));
+    }
 l_lblExit:
+    if (l_pTelemetry)
+    {
+        l_pTelemetry->Release();
+        l_pTelemetry = NULL;
+    }
+
     if (l_pTrace_1)
     {
         l_pTrace_1->Release();
